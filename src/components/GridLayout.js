4d1bfc9d1bedd1316c1058af8bb9fa9004a86d10
edwinnduti <nduti316@gmail.com>
2021-09-24 20:29:00 -0400

    push to master

--- src/components/GridLayout.js
+++ src/components/GridLayout.js
@@ -17,14 +17,12 @@ export const GridLayout = () => {
           <Box tag="header" background="brand" pad="small">
             <Dashboard />
           </Box>
-
           <Box direction="row" justify="left" background="light-2" >
               <AppSidebar />
                 <Box overflow="auto" pad="medium" width="large">
                     <Home />
                 </Box>
           </Box>
-          
           <Box tag="footer" pad="small" background="dark-1">
             footer
           </Box>
